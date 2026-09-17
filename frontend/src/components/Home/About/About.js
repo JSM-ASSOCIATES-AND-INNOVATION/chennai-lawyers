b232349f07193c84c8d35ed442f98166485add90
JSMVALOR <JSMVALOR@users.noreply.github.com>
2026-09-17 07:23:38 +0530

    style: replace About section image

--- frontend/src/components/Home/About/About.js
+++ frontend/src/components/Home/About/About.js
@@ -44,7 +44,7 @@ const About = () => {
                         <div className="about-image-container">
                             <img
                                 className="about-image"
-                                src={`${process.env.PUBLIC_URL}/FirmImage.jpg`}
+                                src={`${process.env.PUBLIC_URL}/Satish-with-foreigh-deligates.jpg`}
                                 alt="Members of the ChennaiLawyers team in discussion"
                                 loading="lazy"
                             />
