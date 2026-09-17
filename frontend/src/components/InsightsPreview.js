6aa94a51df88f02a4048e4434da94a1e1337bc1d
JSMVALOR <JSMVALOR@users.noreply.github.com>
2026-09-17 06:15:28 +0530

    fix: import Link in InsightsPreview.js

--- frontend/src/components/InsightsPreview.js
+++ frontend/src/components/InsightsPreview.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import { useTheme } from './ThemeContext';
 import '../styles/InsightsPreview.css';
 
